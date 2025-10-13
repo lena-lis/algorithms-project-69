8574c7cba2ed7fac6c562a87eeaac450ced2faca
Elena Lisichenok <fjernt.land@gmail.com>
2025-10-13 12:59:07 +0300

    feat: implement full-text search with relevance scoring

--- src/index.js
+++ src/index.js
@@ -3,23 +3,44 @@ export default function search(docs, query) {
     .toLowerCase()
     .replace(/[^\w\s]/g, '')
     .trim()
+    .split(/\s+/)
 
   const selectedDocs = docs.reduce((acc, doc) => {
     if (!doc.text) return acc
 
-    const termAmount = doc.text
+    let uniqueWordsDictionary = {}
+    let termAmount = 0
+
+    const textArray = doc.text
       .toLowerCase()
       .replace(/[^\w\s]/g, '')
       .split(/\s+/)
-      .filter(word => word === term).length
 
-    if (termAmount > 0) {
-      acc.push({ id: doc.id, entries: termAmount })
+    textArray.forEach((word) => {
+      if (term.includes(word)) {
+        if (!Object.hasOwn(uniqueWordsDictionary, word)) {
+          uniqueWordsDictionary[word] = 1
+        }
+
+        termAmount++
+      }
+    })
+
+    const uniqueWordsAmount = Object.keys(uniqueWordsDictionary).length
+
+    if (uniqueWordsAmount > 0) {
+      acc.push({ id: doc.id, uniqueWords: uniqueWordsAmount, entries: termAmount })
     }
     return acc
   }, [])
 
   return selectedDocs
-    .sort((a, b) => b.entries - a.entries)
+    .sort((a, b) => {
+      if (b.uniqueWords !== a.uniqueWords) {
+        return b.uniqueWords - a.uniqueWords
+      }
+
+      return b.entries - a.entries
+    })
     .map(doc => doc.id)
 }
